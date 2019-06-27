d014ff798911bc9f70d126e6cbaa1e47c632ae11
Ito Yoshiaki <ito-yoshiaki@skw.canon-soft.co.jp>
2019-06-27 15:21:05 +0900

    sakura.io data format

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ s.on("connection", ws => {
       msg: '送信してきたクライアントのみに返す',
       data: 123
     });
-    ws.send(msg);
+//    ws.send(msg);
+    ws.send(makeSmapleMsg());
     s.clients.forEach(client => {
       var msgAll = JSON.stringify({
         msg: '接続しているクライアント全てに送信',
@@ -33,3 +34,73 @@ s.on("connection", ws => {
       console.log('I lost a client');
   });
 });
+
+var msg = makeSmapleMsg();
+console.log("<<" + msg + ">>");
+var data = JSON.parse(msg);
+showSmapleMsg(data);
+
+function makeSmapleMsg() {
+  var msg = {
+    type: "channels",
+    datetime: "2017-04-06T07:46:36.005341001Z",
+    module: "XXXX",
+    payload: {
+      channels: [
+        {
+          channel: 0,
+          datetime: "2016-12-24T09:53:03.429432065Z",
+          type: "f",
+          value: 29.07059
+        },
+        {
+          channel: 1,
+          datetime: "2016-12-24T09:53:03.443432065Z",
+          type: "f",
+          value: 0.10620117
+        },
+        {
+          channel: 2,
+          datetime: "2016-12-24T09:53:03.454432065Z",
+          type: "f",
+          value: -0.032958984
+        },
+        {
+          channel: 3,
+          datetime: "2016-12-24T09:53:03.466432065Z",
+          type: "f",
+          value: 0.96362305
+        },
+        {
+          channel: 4,
+          datetime: "2016-12-24T09:53:03.480432065Z",
+          type: "f",
+          value: -2.908397
+        },
+        {
+          channel: 5,
+          datetime: "2016-12-24T09:53:03.492432065Z",
+          type: "f",
+          value: -0.90076333
+        },
+        {
+          channel: 6,
+          datetime: "2016-12-24T09:53:03.504432065Z",
+          type: "f",
+          value: -0.47328246
+        }
+      ]
+    },
+  }
+  return JSON.stringify(msg);
+}
+
+function showSmapleMsg(data) {
+  console.log(data.payload.channels.length);
+  data.payload.channels.forEach(channel => {
+    console.log(channel.channel);
+    console.log(channel.datetime);
+    console.log(channel.type);
+    console.log(channel.value);
+  });
+}
